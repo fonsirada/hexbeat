b1ed6f26aa1f6e86876239f4bfa25472a1823c78
fonsirada <fonsirada13@gmail.com>
2025-04-03 22:52:03 -0400

    decided to start fresh by just making sure the code in class worked with our tilset

--- src/joypad.asm
+++ src/joypad.asm
@@ -1,23 +1,31 @@
 ; header here
 
-include "joypad.inc"
+include "src/joypad.inc"
+include "src/hardware.inc"
+include "src/utils.inc"
 
 
-UpdateSample:
-    halt
+section "joypad", rom0
 
-    ; get the joypad buttons that are being held!
-    ld a, [PAD_CURR]
+; update_joypad:
+;     halt
 
-    ; Is 'a' being held?
-    bit PADB_A, a
-    jr nz, .done_pressing_a
-    ; perform action
-        ; jump
-        ld a, [SPRITE_1_ADDRESS + OAMA_X]
-        inc a
-        ld [SPRITE_1_ADDRESS + OAMA_X], a
-        copy [SPRITE_1_ADDRESS + OAMA_FLAGS], OAMF_PAL0
-    .done_pressing_a
+;     ; get the joypad buttons that are being held!
+;     ld a, [PAD_CURR]
 
-    ret
+;     ; Is 'a' being held?
+;     bit PADB_A, a
+;     jr nz, .done_pressing_a
+;     ; perform action
+;         ; jump
+
+;         ;ld a, [$C000]
+;         ;inc a
+;         ;inc a
+;         ;ld [$C000], a
+;         ;copy [SPRITE_1_ADDRESS + OAMA_FLAGS], OAMF_PAL0
+;     .done_pressing_a
+
+;     ret
+
+; export update_joypad
